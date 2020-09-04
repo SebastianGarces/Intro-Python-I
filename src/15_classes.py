ce5337f5081a6fdfd990acbd6c0a02d6a8725cb8
Sebastian Garces <gsgarces1@gmail.com>
2020-09-04 01:58:04 -0400

    removed __str__ from print

--- src/15_classes.py
+++ src/15_classes.py
@@ -20,7 +20,7 @@ class Waypoint(LatLon):
         self.name = name
 
     def __str__(self):
-        return '\n{ name: ' + self.name+', \n  lat: ' + str(self.lat)+', \n  lon: ' + str(self.lon)+' \n},'
+        return '\n{ name: ' + self.name+', \n  lat: ' + str(self.lat)+', \n  lon: ' + str(self.lon)+' \n}'
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
@@ -47,7 +47,7 @@ waypoint = Waypoint("Catacombs", 41.70505, -121.51521)
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
-print(waypoint.__str__())
+print(waypoint)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
